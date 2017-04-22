40b9675f0fb287eb6a35f15e6a63d82e13f42145
William <william1234562@gmail.com>
2017-04-22 18:04:40 +0800

    1st

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour {
+
+	public int hp = 2;
+
+	public bool isBrick = true;
+
+	void OnTriggerEnter2D(Collider2D otherCollider) {
+
+		ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript> (); 
+
+		if (shot != null) {
+			if (shot.isBrickShot != isBrick) {
+				hp -= shot.damage;
+
+				Destroy (shot.gameObject);
+
+				if (hp <= 0) {
+					Destroy (gameObject);
+				}
+			}
+		}
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
